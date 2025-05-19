25f1065e02d2725ff1451e3359fcb5fa3a85c232
Fatma Abdelhaleem <fatmaabdelhaleem22@gmail.com>
2025-05-19 06:35:03 +0300

    Initial commit for backend

---
+++ models/Question.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const questionSchema = new mongoose.Schema({
+  text: { type: String, required: true },
+  options: [{ text: String, isCorrect: Boolean }],
+  exam: { type: mongoose.Schema.Types.ObjectId, ref: "Exam", required: true },
+});
+
+module.exports = mongoose.model("Question", questionSchema);
